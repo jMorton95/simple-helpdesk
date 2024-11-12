988070c13a85aedf1da6c583d73e5f1c39fccce2
Josh Morton <josh.morton@propeltech.co.uk>
2024-11-12 17:57:16 +0000

    [dev] Create rudimentary login page

--- app/simple_helpdesk/urls.py
+++ app/simple_helpdesk/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
  
 urlpatterns = [
     path("", home_controller.index, name="index"),
-    path("register", auth_controller.register, name="register")
+    path("register", auth_controller.register, name="register"),
+    path("login", auth_controller.login, name="login")
 ]
  
