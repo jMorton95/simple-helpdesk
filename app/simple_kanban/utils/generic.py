a0e4f40d6587276673df2964b51c8c64b54f7e47
Josh Morton <mortson@hotmail.co.uk>
2024-11-30 17:14:50 +0000

    [fix] more renames

--- app/simple_kanban/utils/generic.py
+++ app/simple_kanban/utils/generic.py
@@ -5,6 +5,25 @@ from django.urls import reverse
 from urllib.parse import urlencode
 
 def form_is_valid(request, form) -> bool:
+  """
+    Validates a form and displays any error messages.
+
+    Iterates form errors and adds to Django's Messaging Framework.
+    It then returns the result of the form.
+
+    Args:
+        request: The HTTP request object used to display error messages.
+        form: Any form that inherits from Django's forms.ModelForm.
+
+    Returns:
+        bool: True if the form is valid, otherwise False.
+
+    Example:
+        if form_is_valid(request, my_form):
+            # Continue processing
+        else:
+            # Handle invalid form
+  """
   try:
     if form.errors and form.errors.items and len(form.errors.items()) > 0:
       for _, errors in form.errors.items():
@@ -13,6 +32,7 @@ def form_is_valid(request, form) -> bool:
   except Exception as e:
     print(str(e))
   return form.is_valid()
+
 
 def get_object_if_exists(model_class, object_id) -> Union[bool, object | None]:
   try:
