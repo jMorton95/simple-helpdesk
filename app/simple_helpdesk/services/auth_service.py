e32bc04a4b7f291c92c0f39a142b0d70be5eb1b0
Josh Morton <mortson@hotmail.co.uk>
2024-11-30 11:53:03 +0000

    [dev] extract auth_service.py

---
+++ app/simple_helpdesk/services/auth_service.py
@@ -0,0 +1,26 @@
+from simple_helpdesk.utils.generic import form_is_valid
+from django.contrib.auth import login, logout, update_session_auth_hash
+from django.contrib.auth.models import User
+
+
+class AuthenticationService():
+  def register(request, form):
+    if form_is_valid(request, form):
+      form.save()
+      login(request, User.objects.filter(username = request.POST["email"]).first())
+      return True
+    else:
+      return False
+    
+  def sign_in(request, form):
+     if form_is_valid(request, form):
+      login(request, form.get_user())
+      return True
+     else:
+      return False
+  
+  def change_password(request, form):
+     if form_is_valid(request, form):
+      form.save()
+      update_session_auth_hash(request, form.user)
+      return True
--- app/simple_helpdesk/views/auth.py
+++ app/simple_helpdesk/views/auth.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, update_session_auth_hash
+from simple_helpdesk.services.auth_service import AuthenticationService
 from simple_helpdesk.forms.auth.login_form import LoginForm
 from simple_helpdesk.utils.generic import form_is_valid
 from simple_helpdesk.forms.auth.register_form import RegisterForm
@@ -11,9 +12,7 @@ from django.contrib.auth.models import User
 def register(request):
   if request.method == "POST":
     form = RegisterForm(request.POST)
-    if form_is_valid(request, form):
-      form.save()
-      login(request, User.objects.filter(username = request.POST["email"]).first())
+    if AuthenticationService.register(request, form):
       return redirect("index")
        
   return render(request, "authentication/register.html", {'form': RegisterForm})
@@ -22,8 +21,7 @@ def register(request):
 def sign_in(request):
   if request.method == "POST":
     form = LoginForm(data=request.POST)
-    if form_is_valid(request, form):
-      login(request, form.get_user())
+    if AuthenticationService.sign_in(request, form):
       return redirect("index")
     
   return render(request, "authentication/sign_in.html", {'form': LoginForm})
@@ -33,14 +31,11 @@ def sign_out(request):
   logout(request)
   return redirect("sign_in")
 
-
 @login_required(login_url="/register")
 def change_password(request):
   if request.method == "POST":
     form = ChangePasswordForm(user=request.user, data=request.POST)
-    if form_is_valid(request, form):
-      form.save()
-      update_session_auth_hash(request, form.user)
+    if AuthenticationService.change_password(request, form):
       return redirect("index")
       
   return render(request, "authentication/change_password.html", {'form': ChangePasswordForm(user=request.user)})
