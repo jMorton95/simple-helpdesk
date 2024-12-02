8652363aef14e4cb16cf84d80c6c9d50499c2abe
Josh Morton <josh.morton@propeltech.co.uk>
2024-12-02 16:47:36 +0000

    [dev] Add unit tests for creating project view

--- app/simple_kanban/tests/unit/test_project_create_view.py
+++ app/simple_kanban/tests/unit/test_project_create_view.py
@@ -7,13 +7,27 @@ class CreateProjectViewTest(BaseUnitTest):
   def test_create_project_post(self):
     url = reverse('project_create')
     
-    self.client.post(url, {
+    #Construct a Project Form with SwimlaneFormSet data.
+    data = {
         'name': 'New Project',
-        'description': 'A New Project Description',
-    })
-    
-    self.assertEqual(Project.objects.count(), 1)
+        'description': 'Testing Project',
+        'form-TOTAL_FORMS': '1',
+        'form-INITIAL_FORMS': '0',
+        'form-MIN_NUM_FORMS': '0',
+        'form-MAX_NUM_FORMS': '1000',
+        'form-0-name': 'Swimlane 1',
+        'form-0-sort_order': '1',
+    }
+
+    response = self.client.post(url, data)
+
+    # Assertions
+    self.assertRedirects(response, reverse('index'))
+    self.assertEqual(Project.objects.count(), 2)
     self.assertEqual(Project.objects.last().name, 'New Project')
+    self.assertEqual(Project.objects.last().swimlanes.count(), 1)
+    self.assertEqual(Project.objects.last().swimlanes.last().name, 'Swimlane 1')
+    self.assertEqual(Project.objects.last().swimlanes.last().sort_order, 1)
 
   def test_create_project_get(self):
     url = reverse('project_create')
