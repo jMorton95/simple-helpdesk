3f03099235de139f1e705f9c0fdc16df25aa8c4b
Josh Morton <josh.morton@propeltech.co.uk>
2024-12-03 19:47:02 +0000

    [fix] test runner absolute path

--- app/simple_kanban/tests/custom_test_runner.py
+++ app/simple_kanban/tests/custom_test_runner.py
@@ -1,3 +1,4 @@
+import os
 from django.test.runner import DiscoverRunner
 from unittest import TestLoader, TestSuite
 
@@ -12,9 +13,14 @@ class CustomTestRunner(DiscoverRunner):
     loader = TestLoader()
     suite = TestSuite()
 
+    # Get the absolute path to the tests directory
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    integration_tests = os.path.join(base_dir, "integration")
+    unit_tests = os.path.join(base_dir, "unit")
+
     if not test_labels:
-      suite.addTests(loader.discover(start_dir="simple_kanban/tests/integration", pattern="test*.py"))
-      suite.addTests(loader.discover(start_dir="simple_kanban/tests/unit", pattern="test*.py"))
+      suite.addTests(loader.discover(start_dir=integration_tests, pattern="test*.py"))
+      suite.addTests(loader.discover(start_dir=unit_tests, pattern="test*.py"))
     else:
       suite = super().build_suite(test_labels, extra_tests, **kwargs)
 
