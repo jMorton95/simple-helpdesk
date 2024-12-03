4548fe4b85be2f7225e31cf37b9a0e0e86f8d7f8
Josh Morton <josh.morton@propeltech.co.uk>
2024-12-03 19:50:29 +0000

    [fix] Pathing in CI/CD still broken

--- app/simple_kanban/tests/custom_test_runner.py
+++ app/simple_kanban/tests/custom_test_runner.py
@@ -1,11 +1,13 @@
-import os
 from django.test.runner import DiscoverRunner
 from unittest import TestLoader, TestSuite
+from pathlib import Path
+from django.conf import settings
 
 class CustomTestRunner(DiscoverRunner):
   """
   A custom test runner to include subdirectories for integration and unit tests.
   """
+
   def build_suite(self, test_labels=None, extra_tests=None, **kwargs):
     """
     Build a test suite using test discovery for specified test paths.
@@ -13,14 +15,13 @@ class CustomTestRunner(DiscoverRunner):
     loader = TestLoader()
     suite = TestSuite()
 
-    # Get the absolute path to the tests directory
-    base_dir = os.path.dirname(os.path.abspath(__file__))
-    integration_tests = os.path.join(base_dir, "integration")
-    unit_tests = os.path.join(base_dir, "unit")
+    # Use Path to construct the test directories
+    integration_tests = settings.BASE_DIR / "simple_kanban" / "tests" / "integration"
+    unit_tests = settings.BASE_DIR / "simple_kanban" / "tests" / "unit"
 
     if not test_labels:
-      suite.addTests(loader.discover(start_dir=integration_tests, pattern="test*.py"))
-      suite.addTests(loader.discover(start_dir=unit_tests, pattern="test*.py"))
+      suite.addTests(loader.discover(start_dir=str(integration_tests), pattern="test*.py"))
+      suite.addTests(loader.discover(start_dir=str(unit_tests), pattern="test*.py"))
     else:
       suite = super().build_suite(test_labels, extra_tests, **kwargs)
 
