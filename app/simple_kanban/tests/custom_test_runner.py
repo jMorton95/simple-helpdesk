bb715645d0a695b581574f5c455aec8d9512a11b
Josh Morton <josh.morton@propeltech.co.uk>
2024-12-03 20:13:15 +0000

    [fix] Pathing is hard across environments!

--- app/simple_kanban/tests/custom_test_runner.py
+++ app/simple_kanban/tests/custom_test_runner.py
@@ -1,14 +1,12 @@
-import os
+from pathlib import Path
+import sys
 from django.test.runner import DiscoverRunner
 from unittest import TestLoader, TestSuite
-from pathlib import Path
-from django.conf import settings
 
 class CustomTestRunner(DiscoverRunner):
   """
   A custom test runner to include subdirectories for integration and unit tests.
   """
-
   def build_suite(self, test_labels=None, extra_tests=None, **kwargs):
     """
     Build a test suite using test discovery for specified test paths.
@@ -16,21 +14,20 @@ class CustomTestRunner(DiscoverRunner):
     loader = TestLoader()
     suite = TestSuite()
 
-    # Use Path to construct the test directories
-    tests_root = os.path.join(settings.BASE_DIR, "../simple_kanban/tests")
-    integration_tests = os.path.join(tests_root, "integration")
-    unit_tests = os.path.join(tests_root, "unit")
-    
-    print(f"Integration tests path: {integration_tests}")
-    print(f"Unit tests path: {unit_tests}")
+    project_root = Path(__file__).resolve().parent.parent
+    if str(project_root) not in sys.path:
+        sys.path.insert(0, str(project_root))
 
     if not test_labels:
-      suite.addTests(loader.discover(start_dir=str(integration_tests), pattern="test*.py"))
-      suite.addTests(loader.discover(start_dir=str(unit_tests), pattern="test*.py"))
+        integration_tests = project_root / "tests/integration"
+        unit_tests = project_root / "tests/unit"
+
+        suite.addTests(loader.discover(start_dir=str(integration_tests), pattern="test*.py"))
+        suite.addTests(loader.discover(start_dir=str(unit_tests), pattern="test*.py"))
     else:
-      suite = super().build_suite(test_labels, extra_tests, **kwargs)
+        suite = super().build_suite(test_labels, extra_tests, **kwargs)
 
     if extra_tests:
-      suite.addTests(extra_tests)
+        suite.addTests(extra_tests)
 
     return suite
